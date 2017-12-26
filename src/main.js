0d5ebdb731b3afe640a2b4cade7e34bf5191224f
shiny <shiny_mi@163.com>
2017-12-26 17:35:23 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import ajax from './config/ajax'
+import './style/common'
+import './config/rem'
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+	routes
+})
+
+new Vue({
+	router,
+}).$mount('#app')
---
+++ src/page/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  	<div class="home_container flex-center">
+        <img src="../../images/ball.png" alt="">
+        <p>77%</p>
+        <p>和GoPro一起探索世界...</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+	name: 'home',
+    created(){
+
+    }
+}
+</script>
+<style lang="less">
+    body {
+      background-image: url(../../images/bg1.jpg);
+      background-size: 100% 100%;
+    }
+</style>
